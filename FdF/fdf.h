a7fb5b2ba797b66c909f60509e87b8ae933eddac
dnovak_home <dnovak@student.42.fr>
2024-10-24 11:02:03 +0200

    Loading map from file into struct.

--- FdF/fdf.h
+++ FdF/fdf.h
@@ -6,25 +6,47 @@
 /*   By: dnovak <dnovak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 15:57:02 by dnovak            #+#    #+#             */
-/*   Updated: 2024/10/22 11:18:35 by dnovak           ###   ########.fr       */
+/*   Updated: 2024/10/24 10:56:33 by dnovak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-# define ESCAPE_KEY 65307
+# ifndef ESCAPE_KEY
+#  define ESCAPE_KEY 65307
+# endif
+# ifndef RED
+#  define RED 65536
+# endif
+# ifndef GREEN
+#  define GREEN 256
+# endif
+# ifndef BLUE
+#  define BLUE 1
+# endif
 
 # include "libft/libft.h"
-# include <math.h>
-# include <mlx.h>
-# include <stdlib.h>
+# include <fcntl.h>  // open
+# include <math.h>   // math library
+# include <mlx.h>    // MiniLibX
+# include <stdlib.h> // exit
 
 typedef struct s_point
 {
-	int	alt;
-	int	x;
-	int	y;
-}		t_point;
+	int			alt;
+	double_t	x;
+	double_t	y;
+}				t_point;
+
+typedef struct s_prop
+{
+	int			abscissa;
+	int			ordinate;
+}				t_prop;
+
+// Utils
+double_t		ft_double_max(double_t a, double_t b);
+double_t		ft_double_min(double_t a, double_t b);
 
 #endif // FDF_H
